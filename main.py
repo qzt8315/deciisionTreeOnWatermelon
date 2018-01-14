dd1e2089ec0ca222fcaf0f88511c1e4fdadca0f4
qin <qinzhtao@163.com>
2018-01-14 18:51:30 +0800

    勉强实现决策树，后期进行完善

--- main.py
+++ main.py
@@ -1,67 +1,11 @@
+# coding=utf-8
+import operator
+from math import log
+import time
 import numpy as np
-import math
 
-# 信息熵
-def Ent(D = None, p = None):
-    """
-    :param D: 一维list
-    :param p: 计算概率
-    :return: 返回信息熵
-    """
-    ret = 0.
-    if D is not None:
-        count = len(D) * 1.
-        p1 = {}
-        for i in D:
-            if i in p1.keys():
-                p1[i] += 1
-            else:
-                p1[i] = 1
-        for i in p1.keys():
-            p1[i] /= count
-            ret -= p1[i] * math.log2(p1[i])
-        # 显示计算出来概率
-        # print(p1)
-    elif p is not None:
-        ret -= p * math.log2(p)
-        ret -= (1 - p) * math.log2(1 - p)
-    # 显示计算出来的ret
-    # print(ret)
-    return ret
 
-def Gain(D, a):
-    """
-    :param D: 标签列表，一维行向量
-    :param a: 属性标签列表，一维行向量
-    :return:返回信息增益
-    """
-    ret = Ent(D)
-    # print(ret)
-    count = len(a) * 1.
-    p1 = {}
-    for i in a:
-        if i in p1.keys():
-            p1[i] += 1
-        else:
-            p1[i] = 1
-    for i in p1.keys():
-        p1[i] /= count
-    # 显示计算出来概率
-    # print(p1)
-    for i in set(a):
-        # print(i)
-        t_a = np.array(a)
-        # print(t_a == i)
-        t_D = np.array(D)
-        # print(t_D[t_a == i])
-        # print(Ent(t_D[t_a == i]))
-        # print(p1[i]*Ent(t_D[t_a == i]))
-        # print(ret)
-        ret -= p1[i]*Ent(t_D[t_a == i])
-    return ret
-
-# 程序开始
-if __name__ == "__main__":
+def createDataSet():
     # 从文本文件中读取数据
     f = open('data/data.txt')
     data = []
@@ -75,11 +19,95 @@ if __name__ == "__main__":
     # 测试调整的结果
     # print(data)
     # 获得数据集和标签集
-    dataset = data[:, 1:5].tolist()
-    labelset = data[:, 5].tolist()
-    # 显示测试结果
-    # print(dataset, '\n', labelset)
-    # 测试信息熵的计算函数
-    # print(Ent(p = 2/5.))
-    # 测试信息增益率的函数
-    print(Gain(labelset, np.mat(dataset)[:, 0].T.tolist()[0]))
+    dataset = data[:, 1:].tolist()
+    labels = ['ago', 'income','student','credit']
+    return dataset, labels
+
+
+# 计算香农熵
+def calcShannonEnt(dataSet):
+    numEntries = len(dataSet)
+    labelCounts = {}
+    for feaVec in dataSet:
+        currentLabel = feaVec[-1]
+        if currentLabel not in labelCounts:
+            labelCounts[currentLabel] = 0
+        labelCounts[currentLabel] += 1
+    shannonEnt = 0.0
+    for key in labelCounts:
+        prob = float(labelCounts[key]) / numEntries
+        shannonEnt -= prob * log(prob, 2)
+    return shannonEnt
+
+
+def splitDataSet(dataSet, axis, value):
+    retDataSet = []
+    for featVec in dataSet:
+        if featVec[axis] == value:
+            reducedFeatVec = featVec[:axis]
+            reducedFeatVec.extend(featVec[axis + 1:])
+            retDataSet.append(reducedFeatVec)
+    return retDataSet
+
+
+def chooseBestFeatureToSplit(dataSet):
+    numFeatures = len(dataSet[0]) - 1  # 因为数据集的最后一项是标签
+    baseEntropy = calcShannonEnt(dataSet)
+    bestInfoGain = 0.0
+    bestFeature = -1
+    for i in range(numFeatures):
+        featList = [example[i] for example in dataSet]
+        uniqueVals = set(featList)
+        newEntropy = 0.0
+        for value in uniqueVals:
+            subDataSet = splitDataSet(dataSet, i, value)
+            prob = len(subDataSet) / float(len(dataSet))
+            newEntropy += prob * calcShannonEnt(subDataSet)
+        infoGain = baseEntropy - newEntropy
+        if infoGain > bestInfoGain:
+            bestInfoGain = infoGain
+            bestFeature = i
+    return bestFeature
+
+
+# 因为我们递归构建决策树是根据属性的消耗进行计算的，所以可能会存在最后属性用完了，但是分类
+# 还是没有算完，这时候就会采用多数表决的方式计算节点分类
+def majorityCnt(classList):
+    classCount = {}
+    for vote in classList:
+        if vote not in classCount.keys():
+            classCount[vote] = 0
+        classCount[vote] += 1
+    return max(classCount)
+
+
+def createTree(dataSet, labels):
+    classList = [example[-1] for example in dataSet]
+    if classList.count(classList[0]) == len(classList):  # 类别相同则停止划分
+        return classList[0]
+    if len(dataSet[0]) == 1:  # 所有特征已经用完
+        return majorityCnt(classList)
+    bestFeat = chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel = labels[bestFeat]
+    myTree = {bestFeatLabel: {}}
+    del (labels[bestFeat])
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        subLabels = labels[:]  # 为了不改变原始列表的内容复制了一下
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet,
+                                                               bestFeat, value), subLabels)
+    return myTree
+
+
+def main():
+    data, label = createDataSet()
+    t1 = time.clock()
+    myTree = createTree(data, label)
+    t2 = time.clock()
+    print(myTree)
+    print('execute for ', t2 - t1)
+
+
+if __name__ == '__main__':
+    main()
