4ff4010954fd646b7038fb9eb03030cfb6788f6e
qin <qinzhtao@163.com>
2018-01-14 15:02:06 +0800

    计算信息增益

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def Ent(D = None, p = None):
             p1[i] /= count
             ret -= p1[i] * math.log2(p1[i])
         # 显示计算出来概率
-        print(p1)
+        # print(p1)
     elif p is not None:
         ret -= p * math.log2(p)
         ret -= (1 - p) * math.log2(1 - p)
@@ -36,7 +36,28 @@ def Gain(D, a):
     :return:返回信息增益
     """
     ret = Ent(D)
-
+    # print(ret)
+    count = len(a) * 1.
+    p1 = {}
+    for i in a:
+        if i in p1.keys():
+            p1[i] += 1
+        else:
+            p1[i] = 1
+    for i in p1.keys():
+        p1[i] /= count
+    # 显示计算出来概率
+    # print(p1)
+    for i in set(a):
+        # print(i)
+        t_a = np.array(a)
+        # print(t_a == i)
+        t_D = np.array(D)
+        # print(t_D[t_a == i])
+        # print(Ent(t_D[t_a == i]))
+        # print(p1[i]*Ent(t_D[t_a == i]))
+        # print(ret)
+        ret -= p1[i]*Ent(t_D[t_a == i])
     return ret
 
 # 程序开始
@@ -59,4 +80,6 @@ if __name__ == "__main__":
     # 显示测试结果
     # print(dataset, '\n', labelset)
     # 测试信息熵的计算函数
-    print(Ent(labelset))
+    # print(Ent(p = 2/5.))
+    # 测试信息增益率的函数
+    print(Gain(labelset, np.mat(dataset)[:, 0].T.tolist()[0]))
