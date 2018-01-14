84625230749ade7777e3828947ddbe6dbf5b7a86
qin <qinzhtao@163.com>
2018-01-14 11:35:28 +0800

    初始化项目，添加数据文件，从数据文件中读取到数据

---
+++ main.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+# 程序开始
+if __name__ == "__main__":
+    # 从文本文件中读取数据
+    f = open('data/data.txt')
+    data = []
+    # 按行读取数据
+    line = f.readline()
+    while line:
+        data = np.append(data,[float(i) for i in line.split()])
+        line = f.readline()
+    # 调整矩阵的维数
+    data = data.reshape((14,6))
+    # 测试调整的结果
+    print(data)
